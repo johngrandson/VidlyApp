f373d5d47e28c9103607ecae36a268fb1cef7ac3
João Netto <joaonettopb@hotmail.com>
2019-03-07 15:03:09 -0300

    fixed bugs in filtering

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -6,8 +6,8 @@ const ListGroup = props => {
     return (
         <ul className="list-group">
             {items.map(item =>
-                <li onClick={() => onItemSelect(item)}
-                    key={item[valueProperty]}
+                <li key={item[valueProperty]}
+                    onClick={() => onItemSelect(item)}
                     className={item === selectedItem ? "list-group-item active" : "list-group-item"}>
                     {item[textProperty]}
                 </li>
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -15,7 +15,9 @@ export default class Movies extends Component {
     };
 
     componentDidMount() {
-        this.setState({ movies: getMovies(), genres: getGenres() })
+        const genres = [{ name: 'All Genres' }, ...getGenres()]
+
+        this.setState({ movies: getMovies(), genres })
     }
 
     handleDelete = movie => {
@@ -36,7 +38,7 @@ export default class Movies extends Component {
     }
 
     handleGenreSelect = genre => {
-        this.setState({ selectedGenre: genre });
+        this.setState({ selectedGenre: genre, currentPage: 1 });
     }
 
     render() {
@@ -46,7 +48,9 @@ export default class Movies extends Component {
         if (count === 0)
             return <p>There are no movies in the database.</p>
 
-        const filtered = selectedGenre ? allMovies.filter(m => m.genre._id === selectedGenre._id) : allMovies
+        const filtered = selectedGenre && selectedGenre._id
+            ? allMovies.filter(m => m.genre._id === selectedGenre._id)
+            : allMovies
 
         const movies = paginate(filtered, currentPage, pageSize);
 
